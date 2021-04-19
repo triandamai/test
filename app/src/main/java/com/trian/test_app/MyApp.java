c4e854b527cf7887cf752225fada4f7f8fb8ac72
triandamai <triannurizkillah@gmail.com>
2021-04-19 21:23:28 +0700

    feat(all): add dependecies

---
+++ app/src/main/java/com/trian/test_app/MyApp.java
@@ -0,0 +1,10 @@
+package com.trian.test_app;
+
+import android.app.Application;
+
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
